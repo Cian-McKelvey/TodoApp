506439aeeee3b140cc94582208c9f03e7fd88fe1
Cian-McKelvey <cian.p.mckelvey@gmail.com>
2024-05-10 18:11:58 +0100

    Added the ability to add new todos

--- src/main/java/com/mckelvey/resources/TodoResource.java
+++ src/main/java/com/mckelvey/resources/TodoResource.java
@@ -5,14 +5,14 @@ import com.mckelvey.db.MongoDBUtils;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Path("/todos")
 @Produces(MediaType.APPLICATION_JSON)  // Changes the return types of the methods to json in their http response
@@ -27,10 +27,22 @@ public class TodoResource {
     }
 
     // Database operations methods
-    public void addTodo(Todo todo) {
-        String todoJson = todo.toJsonString();
+
+    @POST
+    @Path("/new")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addTodo(Map<String, Object> jsonData) {
+
+        String userId = (String) jsonData.get("userId");
+        String content = (String) jsonData.get("content");
+
+        Todo newTodo = new Todo(userId, content);
+        String todoJson = newTodo.toJsonString();
         Document todoDocument = Document.parse(todoJson);
         todoCollection.insertOne(todoDocument);
+
+        // Return appropriate status code and message
+        return Response.ok().entity("Processed successfully").build();
     }
 
     // Update todo
