a059337a156c6082fe129ff9d809f31376732913
Cian-McKelvey <cian.p.mckelvey@gmail.com>
2024-05-22 22:28:40 +0100

    Decided on attributes for the account and profile classes

--- src/main/java/com/mckelvey/api/UserAccount.java
+++ src/main/java/com/mckelvey/api/UserAccount.java
@@ -1,5 +1,17 @@
 package com.mckelvey.api;
+/*
+Account Class
+Attributes:
 
+    userId: A uuid generated for the account
+    username: A unique identifier for the user.
+    passwordHash: A hashed version of the user's password for security.
+    email: The user's email address for communication and password recovery.
+    createdAt: Timestamp of when the account was created.
+    lastLogin: Timestamp of the user's last login.
+    isAdmin: Boolean indicating if the user has administrative privileges.
+
+*/
 public class UserAccount {
     // Decide on what user accounts should contain, use what is available from PC part project and add more.
 }
---
+++ src/main/java/com/mckelvey/api/UserProfile.java
@@ -0,0 +1,21 @@
+package com.mckelvey.api;
+
+/*
+Profile Class
+Attributes:
+    account: Reference to the associated Account object.
+    firstName: The user's first name.
+    lastName: The user's last name.
+    dateOfBirth: The user's date of birth.
+    profilePicture: URL or path to the user's profile picture - Store it in azure blob or something.
+    bio: Short biography or description of the user.
+    location: The user's location (country).
+    joined_at: Timestamp of when the profile was created or updated.
+*/
+
+public class UserProfile {
+    // Should have a link to a profile picture.
+    // In the event the application doesn't have a associated picture the default image should be used
+
+
+}
